4928fd2d99122dc416f7ad020f646efe1ccef1f8
Conor <conor@wilxite.com>
2020-01-06 23:13:16 +0000

    initial commit

---
+++ src/sand/browser.cljs
@@ -0,0 +1,17 @@
+(ns sand.browser
+  (:require [reagent.core :as reagent]
+            [sand.view :as view]
+            ))
+
+(defn ^:dev/after-load start 
+  []
+  (js/console.log "start")
+  (reagent/render-component [view/app]
+                            ( .getElementById js/document "app" )))
+
+(defn ^:export init 
+  []
+  (js/console.log "Sand Powering Up!")
+  (start))
+
+
---
+++ src/sand/view.cljs
@@ -0,0 +1,7 @@
+(ns sand.view)
+
+(defn app 
+    []
+    [:main
+        [:h1 "Well done mate!"]
+        [:span "Oh and here's your sand..."]])
