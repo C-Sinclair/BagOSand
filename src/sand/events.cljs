174569d62c3ad1be623453bf355fe9ddc181e4db
Conor <conor@wilxite.com>
2020-01-11 17:16:16 +0000

    all events tests passing

--- src/sand/events.cljs
+++ src/sand/events.cljs
@@ -1,15 +1,15 @@
 (ns sand.events
     (:require [sand.state :refer [app-state]]))
 
-(defn increase-weight 
-    [g]
+(defn increase-weight [g]
+  (if (pos-int? g)
     (swap! app-state assoc :basket/weight 
-        (+ (:basket/weight @app-state) g)))
+        (+ (:basket/weight @app-state) g))))
 
-(defn decrease-weight 
-    [g]
+(defn decrease-weight [g]
+  (if (pos-int? g)
     (let [new-figure (- (:basket/weight @app-state) g)]
-        (if (> new-figure 0)
-            (swap! app-state assoc :basket/weight new-figure)
-            (swap! app-state assoc :basket/weight 0))))
+      (if (> new-figure 0)
+        (swap! app-state assoc :basket/weight new-figure)
+        (swap! app-state assoc :basket/weight 0)))))
 
