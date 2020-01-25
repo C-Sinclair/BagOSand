5e3c8670f8cc28d0b9760f88b891382d3475092e
Conor <conor@wilxite.com>
2020-01-25 21:59:02 +0000

    Added cljss for styles

--- src/sand/browser.cljs
+++ src/sand/browser.cljs
@@ -3,18 +3,12 @@
             [sand.routes :as routes]
             [sand.view :as view]))
 
-(defn ^:dev/after-load start 
-  []
-  (js/console.log "s")
+(defn ^:dev/after-load start []
   (routes/app-routes)
   (reagent/render [routes/current-page]
                   (.getElementById js/document "app" )))
 
-(defn ^:export init 
-  []
+(defn ^:export init []
   (js/console.log "Sand Powering Up!")
   (start))
 
-(defn res 
-  []
-  "Here you go!")
--- src/sand/pages/login.cljs
+++ src/sand/pages/login.cljs
@@ -1,5 +1,37 @@
-(ns sand.pages.login)
+(ns sand.pages.login
+  (:require-macros [cljss.core :refer [defstyles]])
+  (:require [reagent.core :as reagent :refer [atom]]
+            [cljss.core :as css]
+            [sand.state :as state]))
+
+(def white "#fff")
+(def light-blue "#6f42c1")
+
+(defstyles button []
+  {:background-color white
+   :color light-blue
+   :&:hover {:background-color light-blue
+             :color white
+             :box-shadow "inset 1px 1px black"}})
+
+(def user-credentials
+  (atom {:email ""
+         :password ""}))
+
+(defn update-email [email]
+   (swap! user-credentials assoc :email email))
+
+(defn update-password [password]
+  (swap! user-credentials assoc :password password))
 
 (defn login []
   [:div [:h1 "Login Page"]
+   [:form
+    [:div [:label "Email"]
+          [:input {:type "text"
+                   :on-change #(update-email (-> % .-target .-value))}]]
+    [:div [:label "Password"]
+     [:input {:type "password"
+              :on-change #(update-password (-> % .-target .-value))}]]
+    [:button {:class (button)} "Login"]]
    [:a {:href "#/"} "Go home"]])
